99d24b6f617a2a35c049a6f559818a6e60f49507
Kamil Bobrowski <kamil.bobrowski.leocode.com>
2023-07-04 03:39:36 +0200

    Created not-found 404

---
+++ src/app/not-found.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { CallToActionButton } from "@/components/buttons";
+import { Text } from "@/components/text";
+import { FlexColumn } from "@/components/wrappers/columns";
+import { colors } from "@/styles/colors";
+
+export default function NotFound() {
+  return (
+    <FlexColumn
+      items={"center"}
+      justify={"center"}
+      styles={`background-color: ${colors.background.ebonyClay}; height: 100vh;`}
+    >
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <Text text={"4"} color={colors.white} size={"notFound"} />
+        <Text text={"0"} color={colors.greenThemeColor} size={"notFound"} />
+        <Text text={"4"} color={colors.white} size={"notFound"} />
+      </div>
+
+      <Text
+        text={"Page Not Found"}
+        color={colors.greenThemeColor}
+        size={"36px"}
+      />
+
+      <FlexColumn
+        items={"center"}
+        justify={"center"}
+        styles="width: 100vh; margin-top: 1rem"
+      >
+        <Text
+          text={"If you have great idea about new page"}
+          color={colors.white}
+          size={"20px"}
+        />
+        <Text
+          text={"send mail with this idea to me!"}
+          color={colors.white}
+          size={"20px"}
+        />
+      </FlexColumn>
+
+      <CallToActionButton name={"Contact Form"} href={"/"} />
+    </FlexColumn>
+  );
+}
