93644e241bfcf7db81a7946703ed8acec41fd00a
Kamil Bobrowski <kamil.bobrowski.leocode.com>
2023-07-04 20:53:15 +0200

    Created BlogSection

--- src/app/BlogSection.tsx
+++ src/app/BlogSection.tsx
@@ -1,3 +1,64 @@
+import { CallToActionButton } from "@/components/buttons";
+import { ShortTileInfo } from "@/components/shortTileInfo";
+import { Text } from "@/components/text";
+import { Column, FlexColumn } from "@/components/wrappers/columns";
+import { colors } from "@/styles/colors";
+
+const threeLastArticles = [
+  {
+    title: "Pro tips in JavaScripts",
+    short:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    href: "/blog",
+  },
+  {
+    title: "Burnout ?!",
+    short:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    href: "/blog",
+  },
+  {
+    title: "Soft skills in IT",
+    short:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    href: "/blog",
+  },
+];
+
 export const BlogSection = () => {
-  return <div>BlogSection</div>;
+  return (
+    <FlexColumn
+      items="center"
+      justify="center"
+      styles="height: 100vh;  width: 100%"
+      minusLayoutMargin
+    >
+      <Text text={"Blog"} color={colors.greenThemeColor} size={"36px"} />
+      <Text
+        text={"Latest Articles"}
+        color={colors.white}
+        size={"48px"}
+        styles="margin-bottom: 3.5rem"
+      />
+
+      <Column numberOfColumns={3} gap={50}>
+        {threeLastArticles.map((article) => {
+          return (
+            <ShortTileInfo
+              key={article.title}
+              title={article.title}
+              href={article.href}
+              shortInfo={article.short}
+            />
+          );
+        })}
+      </Column>
+
+      <CallToActionButton
+        name="All Articles"
+        href="/blog"
+        styles="margin-top: 5rem"
+      />
+    </FlexColumn>
+  );
 };
