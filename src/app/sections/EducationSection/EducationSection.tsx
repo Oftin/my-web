087bfc36e2fd6601c13af5b80ccd95e778ab01b7
Kamil Bobrowski <kamil.bobrowski.leocode.com>
2023-07-27 08:02:36 +0200

    Adjust EducationSection to RWD

--- src/app/sections/EducationSection/EducationSection.tsx
+++ src/app/sections/EducationSection/EducationSection.tsx
@@ -1,18 +1,17 @@
 import { PagePosition } from "@/components/wrappers/pagePosition";
 import { CallToActionButton } from "@/components/buttons";
-import { WhiteAndGreenText, GreenAndWhiteText, Text } from "@/components/text";
 import { colors } from "@/styles/colors";
-import { Icons } from "@/components/icons";
 import { Images } from "@/components/images";
 import { Image } from "@/components/Modifiers";
-import { Arrow } from "@/components/arrow";
 import {
   EducationSectionWrapper,
   TitleWrapper,
-  ArrowWrapper,
-  ArrowPosition,
   StudyTime,
-  ContentWrapper,
+  MainText,
+  SecondText,
+  TextParagraph,
+  StudyStage,
+  ImageWithButtonWrapper,
 } from "./styled";
 import { EducationSectionProps } from "./types";
 
@@ -23,137 +22,79 @@ export const EducationSection = ({
     <PagePosition backgroundColor={backgroundColor} paddingPlus id="education">
       <EducationSectionWrapper>
         <TitleWrapper>
-          <Text text="University" color={colors.greenThemeColor} size="32px" />
-          <Text text="Education" color={colors.white} size="48px" />
+          <SecondText>University</SecondText>
+          <MainText>Education</MainText>
         </TitleWrapper>
 
-        <ContentWrapper>
-          <ArrowWrapper>
-            <ArrowPosition>
-              <Arrow orientation="vertical" showArrowhead={false} />
-            </ArrowPosition>
-            <div>
-              <Image src={Icons.GreenDotWithRing} alt="" />
-            </div>
-          </ArrowWrapper>
+        <StudyTime>
+          <StudyStage>
+            <TextParagraph>
+              Silesian University of Technology in Gliwice (Poland)
+            </TextParagraph>
 
-          <StudyTime>
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-              }}
+            <TextParagraph
+              $firsrColor={colors.greenThemeColor}
+              $secondColor={colors.white}
             >
-              <Text
-                text="Silesian University of Technology in Gliwice (Poland)"
-                color={colors.white}
-                size="20px"
-              />
+              <div>2016-10 - 2020-02</div>
+              <div>Bachelor of Engineering</div>
+            </TextParagraph>
 
-              <GreenAndWhiteText
-                firstText="2016-10 - 2020-02"
-                secondText="Bachelor of Engineering"
-                textSize="20px"
-                styles="margin-top: 0.75rem; margin-bottom: 0.75rem"
-              />
-
-              <WhiteAndGreenText
-                firstText="Faculty of Mechanical Engineering -"
-                secondText="Automation and Robotics"
-                textSize="20px"
-              />
-            </div>
-
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
-                marginTop: "4rem",
-              }}
+            <TextParagraph
+              $firsrColor={colors.white}
+              $secondColor={colors.greenThemeColor}
             >
-              <Image src={Images.EngineeringRobot} alt={"EngineeringRobot"} />
+              <div>Faculty of Mechanical Engineering -</div>
+              <div>Automation and Robotics</div>
+            </TextParagraph>
+          </StudyStage>
 
-              <Text
-                text="Engineering degree project"
-                color={colors.white}
-                size="16px"
-                styles="margin-top: 1rem"
-              />
+          <ImageWithButtonWrapper>
+            <Image src={Images.EngineeringRobot} alt={"EngineeringRobot"} />
 
-              <CallToActionButton
-                name="Read More"
-                href="/education/engineering-degree-project#top"
-                styles="margin-top: 1rem"
-              />
-            </div>
-          </StudyTime>
-        </ContentWrapper>
+            <TextParagraph>Engineering degree project</TextParagraph>
 
-        <ContentWrapper>
-          <ArrowWrapper>
-            <ArrowPosition>
-              <Arrow orientation="vertical" />
-            </ArrowPosition>
-            <div>
-              <Image src={Icons.GreenDotWithRing} alt="GreenDotWithRing" />
-            </div>
-          </ArrowWrapper>
+            <CallToActionButton
+              name="Read More"
+              href="/education/engineering-degree-project#top"
+            />
+          </ImageWithButtonWrapper>
+        </StudyTime>
 
-          <StudyTime>
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-              }}
+        <StudyTime>
+          <StudyStage>
+            <TextParagraph>
+              Silesian University of Technology in Gliwice (Poland)
+            </TextParagraph>
+
+            <TextParagraph
+              $firsrColor={colors.greenThemeColor}
+              $secondColor={colors.white}
             >
-              <Text
-                text="Silesian University of Technology in Gliwice (Poland)"
-                color={colors.white}
-                size="20px"
-              />
+              <div>2020-03 - 2021-11</div>
+              <div>Master of Engineering</div>
+            </TextParagraph>
 
-              <GreenAndWhiteText
-                firstText="2020-03 - 2021-11"
-                secondText="Master of Engineering"
-                textSize="20px"
-                styles="margin-top: 0.75rem; margin-bottom: 0.75rem"
-              />
-
-              <WhiteAndGreenText
-                firstText="Faculty of Mechanical Engineering -"
-                secondText="Automation and Robotics"
-                textSize="20px"
-              />
-            </div>
-
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
-                marginTop: "4rem",
-              }}
+            <TextParagraph
+              $firsrColor={colors.white}
+              $secondColor={colors.greenThemeColor}
             >
-              <Image src={Images.MasterSpider} alt={"MasterSpider"} />
+              <div>Faculty of Mechanical Engineering -</div>
+              <div>Automation and Robotics</div>
+            </TextParagraph>
+          </StudyStage>
 
-              <Text
-                text="Master degree project"
-                color={colors.white}
-                size="16px"
-                styles="margin-top: 1rem"
-              />
+          <ImageWithButtonWrapper>
+            <Image src={Images.MasterSpider} alt={"MasterSpider"} />
 
-              <CallToActionButton
-                name="Read More"
-                href="/education/master-degree-project#top"
-                styles="margin-top: 1rem;"
-              />
-            </div>
-          </StudyTime>
-        </ContentWrapper>
+            <TextParagraph>Master degree project</TextParagraph>
+
+            <CallToActionButton
+              name="Read More"
+              href="/education/master-degree-project#top"
+            />
+          </ImageWithButtonWrapper>
+        </StudyTime>
       </EducationSectionWrapper>
     </PagePosition>
   );
--- src/app/sections/EducationSection/styled.ts
+++ src/app/sections/EducationSection/styled.ts
@@ -1,4 +1,6 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { typography } from "@/styles/typography";
+import { colors } from "@/styles/colors";
 
 export const EducationSectionWrapper = styled.div`
   display: flex;
@@ -16,46 +18,104 @@ export const TitleWrapper = styled.div`
   }
 `;
 
-export const ArrowWrapper = styled.div`
+export const StudyStage = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100%;
-`;
-
-export const ArrowPosition = styled.div`
-  position: absolute;
-  height: 60%;
-  margin-top: 4rem;
-
-  @media only screen and (max-width: 1300px) {
-    height: 60%;
-  }
+  flex-direction: column;
 `;
 
 export const StudyTime = styled.div`
-  display: grid;
-  grid-template-columns: 70% 30%;
+  display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   width: 100%;
   margin-top: 4rem;
   margin-bottom: 4rem;
+`;
 
-  @media only screen and (max-width: 1300px) {
-    grid-template-columns: 1fr;
+export const MainText = styled.div`
+  display: flex;
+  color: ${colors.white};
+
+  @media only screen and (max-width: 575px) {
+    ${typography.titleText["28px"]}
+    gap: 6px;
+  }
+
+  @media only screen and (min-width: 576px) and (max-width: 767px) {
+    ${typography.titleText["36px"]}
+    gap: 8px;
+  }
+
+  @media only screen and (min-width: 768px) {
+    ${typography.titleText["48px"]}
+    gap: 10px;
   }
 `;
 
-export const ContentWrapper = styled.div`
-  display: grid;
-  grid-template-columns: 15% 1fr;
-  width: 100%;
-  height: 100%;
+export const SecondText = styled.div`
+  display: flex;
+  color: ${colors.greenThemeColor};
+
+  @media only screen and (max-width: 575px) {
+    ${typography.titleText["18px"]}
+    gap: 6px;
+  }
+
+  @media only screen and (min-width: 576px) and (max-width: 767px) {
+    ${typography.titleText["24px"]}
+    gap: 8px;
+  }
+
+  @media only screen and (min-width: 768px) {
+    ${typography.titleText["32px"]}
+    gap: 10px;
+  }
+`;
+
+export const ImageWithButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+  width: 80vw;
+  max-width: 1200px;
+  margin-top: 2rem;
+  gap: 20px;
+`;
 
-  @media only screen and (max-width: 1300px) {
-    grid-template-columns: 15% 1fr 15%;
+export const TextParagraph = styled.div<{
+  $firsrColor?: string;
+  $secondColor?: string;
+}>`
+  display: flex;
+  color: ${colors.white};
+
+  ${({ $firsrColor }) =>
+    $firsrColor &&
+    css`
+      color: ${$firsrColor};
+    `}
+
+  :last-child {
+    ${({ $secondColor }) =>
+      $secondColor &&
+      css`
+        color: ${$secondColor};
+      `}
+  }
+
+  @media only screen and (max-width: 575px) {
+    ${typography.text.bold["12px"]}
+    gap: 6px;
+  }
+
+  @media only screen and (min-width: 576px) and (max-width: 767px) {
+    ${typography.text.bold["16px"]}
+    gap: 8px;
+  }
+  @media only screen and (min-width: 768px) {
+    ${typography.text.bold["20px"]}
+    gap: 10px;
   }
 `;
