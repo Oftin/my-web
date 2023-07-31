072afc9cbd60a036f010724ac0c0bd612912b7e3
Kamil Bobrowski <kamil.bobrowski.leocode.com>
2023-08-01 00:24:40 +0200

    Change history.back() method

--- src/components/buttons/ArrowBack.tsx
+++ src/components/buttons/ArrowBack.tsx
@@ -1,13 +1,10 @@
 import Image from "next/image";
-import { useRouter } from "next/navigation";
 import { Icons } from "@/components/icons";
 import { ArrowBackWrapper } from "./styled";
 
 export const ArrowBack = () => {
-  const router = useRouter();
-
   return (
-    <ArrowBackWrapper onClick={() => router.back()}>
+    <ArrowBackWrapper onClick={() => history.back()}>
       <Image src={Icons.ArrowBack} alt="Back Arrow" width={75} height={75} />
     </ArrowBackWrapper>
   );
