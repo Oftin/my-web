d32f0c1ac3579e3cd4aa030a0a6ebfe319c78807
Kamil Bobrowski <kamil.bobrowski.leocode.com>
2023-08-01 00:52:26 +0200

    wip

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { usePathname } from "next/navigation";
 import { slide as Menu } from "react-burger-menu";
@@ -30,18 +31,18 @@ const webTabs = [
 
 export const Navbar = ({ showArrowBack = true }: NavbarProps) => {
   const [openHamburgerMenu, setOpenHamburgerMenu] = useState<boolean>(false);
-  const [windowWidth, setWindowWidth] = useState<number>(0);
+  // const [windowWidth, setWindowWidth] = useState<number>(0);
 
   const router = useRouter();
 
-  useEffect(() => {
-    function handleResize() {
-      setWindowWidth(window.innerWidth);
-    }
-    window.addEventListener("resize", handleResize);
-    handleResize();
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+  // useEffect(() => {
+  //   function handleResize() {
+  //     setWindowWidth(window.innerWidth);
+  //   }
+  //   window.addEventListener("resize", handleResize);
+  //   handleResize();
+  //   return () => window.removeEventListener("resize", handleResize);
+  // }, []);
 
   const currentPage = usePathname();
   const mainScreen = currentPage === "/";
@@ -55,9 +56,9 @@ export const Navbar = ({ showArrowBack = true }: NavbarProps) => {
     );
   };
 
-  const turnOnHamburgerMenu = () => {
-    return windowWidth <= 992;
-  };
+  // const turnOnHamburgerMenu = () => {
+  //   return windowWidth <= 992;
+  // };
 
   return (
     <>
@@ -91,7 +92,7 @@ export const Navbar = ({ showArrowBack = true }: NavbarProps) => {
             )}
           </LeftSideLi>
 
-          {turnOnHamburgerMenu() ? (
+          {true ? (
             <RightSideLiVertical>
               <Menu
                 right
